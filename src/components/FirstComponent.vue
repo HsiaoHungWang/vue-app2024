1b3ea3e6eb001699a630b1fde5f6eb8dbd40815d
HsiaoHungWang <wang@ispan.com.tw>
2024-12-19 16:09:24 +0800

    setup 語法糖的介紹

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,9 +1,21 @@
 <template>
-    <h2>第一個 Component</h2>
+    <h2 id="myH">第一個 Component</h2>
 </template>
 <style scoped>
   h2{
     background-color: silver;
   }
 </style>
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+
+//表示 DOM Tree 已經產生，可以用 DOM API 及 jQuery 程式去控制它了
+onMounted(()=>{
+    //DOM API、jQuery
+    const theH = document.querySelector('#myH')
+    console.log(theH)  //theH.textContent
+    console.log(theH.textContent)
+})
+
+
+</script>
--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -1,10 +1,30 @@
+<!-- <script>
+import { ref } from 'vue';
+
+
+    export default{
+       //options api
+        // data(){
+        //    return {message:"Options API"} 
+        // }
+
+       //compostion api
+       setup(){
+        const message = ref("Composition API")
+        return {message}
+       }
+    }
+</script> -->
+
 <script setup>
-    
+  import { ref } from 'vue';
+  const message = ref("setup 語法糖")
 </script>
 
 <template>
     <div>
-<h2>第二個 Component</h2>
+      <input type="text" v-model="message" />
+      <h2>第二個 Component {{ message }}</h2>
     </div>
 </template>
 

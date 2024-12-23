b39362ec4efaa591abda13b931de0cfc67435bc4
HsiaoHungWang <wang@ispan.com.tw>
2024-12-23 11:24:47 +0800

    Text Interpolation 插值不能使用流程控制：if…else…、switch、try…catch… 迭代：for、for…of…、while 變數宣告：var、const、let 建立函式：function

--- src/components/BindingComponent.vue
+++ src/components/BindingComponent.vue
@@ -2,10 +2,14 @@
 import { ref } from 'vue';
 
     const message = ref("Text Interpolation")
-
+    const isLogin = ref(false)
+    const i = 1
     const sayHi = ()=>{
         return "Hi"
     }
+
+    const week = ref(["日", "一", "二", "三", "四", "五", "六"])
+    console.log(new Date().getDay())
 </script>
 
 <template>
@@ -13,9 +17,23 @@ import { ref } from 'vue';
 <h2>Template Binding</h2>
     <!--{{ Text Interpolation 插值 表達式 Expression  }}  -->
      <p>{{ 2 }}</p>
-     <p>{{ message }}</p>
+     <p>{{ `Hello ${message}`  }}</p>
      <p>{{ 1 + 2 }}</p>
      <p>{{ sayHi() }}</p>
+     <p>{{ false ? "YES" : "NO" }}</p>
+     <p>{{ isLogin ? "登入帳號" : "請先登入" }}</p>
+
+     <!--錯誤寫法，須改用三元運算子-->
+     <!-- {{ if (true) { return 'Hello' } }} -->
+       <!--錯誤寫法，不能做變數需告 -->
+     <!--{{ let name = "Jack"  }}-->
+{{i+1}}
+
+     <p>{{ new Date().toLocaleDateString()  }}</p>
+
+     <div>星期{{      }}</div>
+
+
     </div>
 </template>
 

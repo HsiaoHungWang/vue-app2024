53b3f09dd8a88b5b9006e2a05c48042de2b6065f
HsiaoHungWang <wang@ispan.com.tw>
2024-12-23 15:55:58 +0800

    computed 說明

--- src/components/ComputedDemo.vue
+++ src/components/ComputedDemo.vue
@@ -1,16 +1,28 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
     const celsius = ref(0)
+    const TempatureConvert = ()=>{  
+        console.log('TempatureConvert')    
+        return (celsius.value * 9 / 5) + 32
+    }
+    // const fahrenheit = computed(()=>(celsius.value * 9 / 5) + 32)
+    const fahrenheit = computed(()=>{
+        console.log('computed')
+       return (celsius.value * 9 / 5) + 32
+    })
 </script>
 
 <template>
     <div>
 <h2>Computed Demo</h2>
 <div>
-攝氏：<input type="text" v-model="celsius" /></div>
+攝氏：<input type="text" v-model.lazy.number="celsius" /></div>
 <div>華氏：{{ (celsius * 9 / 5) + 32 }}</div>
-    </div>
+<div>華氏(function)：{{ TempatureConvert()}} | {{ TempatureConvert()}} | {{ TempatureConvert()}}</div>
+<div>華氏(computed)：{{ fahrenheit }} | {{ fahrenheit }} | {{ fahrenheit }}</div>
+
+   </div>
 </template>
 
 <style lang="css" scoped>

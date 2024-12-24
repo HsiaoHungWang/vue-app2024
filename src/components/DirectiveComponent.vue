49bd2890f7a9a6a7158a22712a2bfe38f72b869c
HsiaoHungWang <wang@ispan.com.tw>
2024-12-24 15:08:17 +0800

    v-if v-else

--- src/components/DirectiveComponent.vue
+++ src/components/DirectiveComponent.vue
@@ -2,10 +2,11 @@
 import { ref } from 'vue';
 
     const message = ref("<h3>directive v-text</h3>")
-    const isLoading = ref(true)
-    setTimeout(()=>{
-       isLoading.value = false
-    },3000)
+    const isLoading = ref(false)
+    const isLogin = ref(true)
+    // setTimeout(()=>{
+    //    isLoading.value = false
+    // },3000)
 </script>
 
 <template>
@@ -15,6 +16,15 @@ import { ref } from 'vue';
 <div v-text="message"></div>
 <div v-html="message"></div>
 <img src="/images/loading.gif" alt="loading" v-show="isLoading" />
+<img src="/images/loading.gif" alt="loading" v-if="isLoading" />
+<div v-if="isLogin">
+    <p>isLogin 為 true</p>
+    <button @click="isLogin = !isLogin">登出</button>
+</div>
+<div v-else>
+    <p>isLogin 為 false</p>
+    <button @click="isLogin = !isLogin">登入</button>
+</div>
     </div>
 </template>
 

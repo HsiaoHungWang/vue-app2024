fbc49fd893f14bc45f7b6af2668cdabafb602558
HsiaoHungWang <wang@ispan.com.tw>
2024-12-24 10:47:30 +0800

    vue-router 的使用

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,24 @@ import HomeView from './views/HomeView.vue';
 
 </script>
 
-<template>
+<template>    
  <h2>Hello Vue!!!!!</h2>
- <HomeView></HomeView>
+ <RouterLink to="/">首頁</RouterLink> | 
+ <RouterLink to="/about">關於我們</RouterLink> | 
+ <RouterLink to="/contact">聯絡我們</RouterLink> | 
+ <hr />
+ <!--是一個容器，根據路由，載入不同的Component-->
+ <RouterView></RouterView>
+
+
+
+
+
+<!-- <HomeView></HomeView>
  <AboutView></AboutView>
  <ContactView></ContactView>
-  <!--<BMIComponent></BMIComponent>
+ 
+  <BMIComponent></BMIComponent>
 <WatchDemo></WatchDemo>
  <ComputedDemo></ComputedDemo>
 <BindingComponent></BindingComponent>
---
+++ src/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "./views/HomeView.vue";
+import AboutView from "./views/AboutView.vue";
+import ContactView from "./views/ContactView.vue";
+
+//路由設定
+//URL => Component
+const routes = [
+    //http://localhost:5173/ => HomeView.vue
+    { path: "/", component: HomeView },
+    //http://localhost:5173/about => AboutView.vue
+    { path: "/about", component: AboutView },
+    //http://localhost:5173/contact => ContactView.vue
+    { path: "/contact", component: ContactView },
+    // { path: "/register", component: RegisterView }
+]
+
+const router = createRouter({
+    history: createWebHistory(),//指定用哪一種方式產生歷史紀錄
+    routes //指定路由設定，參數名稱跟路由設定的變數名稱一樣
+})
+
+export default router

0b95aec50181810129e17fe857b5ecaa0672ea7d
HsiaoHungWang <wang@ispan.com.tw>
2024-12-24 12:01:14 +0800

    使用bootstrap製作導覽列

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <script setup>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 import BindingComponent from './components/BindingComponent.vue';
 import BMIComponent from './components/BMIComponent.vue';
 import ComputedDemo from './components/ComputedDemo.vue';
@@ -10,19 +14,20 @@ import WatchDemo from './components/WatchDemo.vue';
 import AboutView from './views/AboutView.vue';
 import ContactView from './views/ContactView.vue';
 import HomeView from './views/HomeView.vue';
+import NavbarComponent from './components/NavbarComponent.vue';
 
 
 
 </script>
 
 <template>    
- <h2>Hello Vue!!!!!</h2>
+<div class="container">
+    <!-- <h2>Hello Vue!!!!!</h2> -->
+    <NavbarComponent></NavbarComponent>
  <!-- <RouterLink to="/">首頁</RouterLink> | 
  <RouterLink to="/about">關於我們</RouterLink> | 
  <RouterLink to="/contact">聯絡我們</RouterLink> |  -->
- <RouterLink :to="{name:'home'}">首頁</RouterLink> | 
- <RouterLink :to="{name:'about'}">關於我們</RouterLink> | 
- <RouterLink :to="{name:'contact'}">聯絡我們</RouterLink> | 
+
  <hr />
  <!--是一個容器，根據路由，載入不同的Component-->
  <RouterView></RouterView>
@@ -42,6 +47,8 @@ import HomeView from './views/HomeView.vue';
  <MyComponent></MyComponent>
  <FirstComponent></FirstComponent>
 <SecondComponent></SecondComponent>  -->
+</div>
+
 
 </template>
 

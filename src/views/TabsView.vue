1e2441ed9473049c520a4d12a5fc063973097f27
HsiaoHungWang <wang@ispan.com.tw>
2024-12-26 14:01:52 +0800

    透過內建component元素製作頁籤的效果

--- src/views/TabsView.vue
+++ src/views/TabsView.vue
@@ -1,7 +1,16 @@
 <script setup>
+import BMIComponent from '@/components/BMIComponent.vue';
 import FirstComponent from '@/components/FirstComponent.vue';
 import SecondComponent from '@/components/SecondComponent.vue';
 import { ref } from 'vue';
+
+
+const tabs = ref([
+    { name: 'First', component: FirstComponent },
+    { name: 'Second', component: SecondComponent },
+    { name: 'BMI', component: BMIComponent }
+])
+const currentTab = ref(FirstComponent)
 
 const items = [FirstComponent, SecondComponent]
 const activeIndex = ref(0)
@@ -10,6 +19,14 @@ const activeIndex = ref(0)
 <template>
     <div>
         <h2>Tabs</h2>
+        <ul class="nav nav-tabs">
+            <li class="nav-item" v-for="tab in tabs" :key="tab.name">
+                <a class="nav-link active" href="#" @mouseenter="currentTab = tab.component">{{ tab.name }}</a>
+            </li>
+        </ul>
+        <component :is="currentTab"></component>
+
+        <hr />
         <select v-model="activeIndex">
             <option value="0">First</option>
             <option value="1">Second</option>

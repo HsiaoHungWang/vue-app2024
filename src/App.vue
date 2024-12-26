e744f64811a1b7a9201f1670848c8fccc48aa405
HsiaoHungWang <wang@ispan.com.tw>
2024-12-26 13:46:30 +0800

    CSS 的 transform transition

--- src/App.vue
+++ src/App.vue
@@ -22,27 +22,28 @@ import EventModifiers from './components/EventModifiers.vue';
 
 </script>
 
-<template>    
-<div class="container">
-    <!-- <h2>Hello Vue!!!!!</h2> -->
-    <NavbarComponent></NavbarComponent>
- <!-- <RouterLink to="/">首頁</RouterLink> | 
+<template>
+    <div class="container">
+        <!-- <h2>Hello Vue!!!!!</h2> -->
+        <NavbarComponent></NavbarComponent>
+        <!-- <RouterLink to="/">首頁</RouterLink> | 
  <RouterLink to="/about">關於我們</RouterLink> | 
  <RouterLink to="/contact">聯絡我們</RouterLink> |  -->
 
- <hr />
- <!--是一個容器，根據路由，載入不同的Component-->
- <RouterView></RouterView>
+        <hr />
+        <!--是一個容器，根據路由，載入不同的Component-->
+        <RouterView></RouterView>
 
-<hr />
-<div>
-    <!-- <DirectiveComponent></DirectiveComponent>
+        <hr />
+        <div>
+            <!-- <DirectiveComponent></DirectiveComponent>
      <EventModifiers></EventModifiers> -->
-</div>
+            <img class="img1" src="/images/loading.gif" alt="loading" />
+        </div>
 
 
 
-<!-- <HomeView></HomeView>
+        <!-- <HomeView></HomeView>
  <AboutView></AboutView>
  <ContactView></ContactView>
  
@@ -55,11 +56,26 @@ import EventModifiers from './components/EventModifiers.vue';
 <SecondComponent></SecondComponent>  
 <BindingComponent></BindingComponent>-->
 
-</div>
+    </div>
 
 
 </template>
 
 <style scoped>
-h2{color:green}
+h2 {
+    color: green
+}
+
+.img1 {
+    /* scaleX(-1) rotate(180deg); */
+    transform: translateX(0px) rotate(0deg);
+    opacity: 1;
+}
+
+.img1:hover {
+    transform: translateX(300px) rotate(360deg);
+    opacity: 0;
+
+    transition: all 3s;
+}
 </style>
